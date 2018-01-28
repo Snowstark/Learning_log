11a9c545e45b4019e32ddefccec5bc6cf89a7d98
式子墨 <q17010112@njupt.edu.cn>
2018-01-28 21:20:00 +0800

    Initial commit.

---
+++ Learning_logs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Learning_logs.models import Topic
+from Learning_logs.models import Entry
+
+admin.site.register(Topic)
+admin.site.register(Entry)
---
+++ Learning_logs/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """用户学习的主题"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+
+class Entry(models.Model):
+    """具体相关"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # 复数显示的名字
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text[:50] + "..."
