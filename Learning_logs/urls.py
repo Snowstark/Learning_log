2d23df01f2d4868cc093cf8ab7b691b455bc99ba
式子墨 <q17010112@njupt.edu.cn>
2018-01-29 22:41:01 +0800

    Second commit.

---
+++ Learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""定义Learning_logs的url模式"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # Main page
+    url(r'^$', views.index, name='index'),
+
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+]
--- Learning_logs/views.py
+++ Learning_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+
+def index(request):
+    """Page of Learning_log"""
+    return render(request, 'Learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'Learning_logs/topics.html', context)
